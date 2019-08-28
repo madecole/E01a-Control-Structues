a76f4cd0818a94d95f6fa34922b04e4eb9c84b17
Cole <madecole@iu.edu>
2019-08-28 18:44:09 -0400

    FinishesQuestion8

--- main8.py
+++ main8.py
@@ -9,7 +9,7 @@ utils.clear()                       # clear the screen
 print('Greetings!')
 color = ''
 while (color != 'red'):
-    color = input("What is my favorite color? ")
+     color = input("What is my favorite color? ")
     color = color.lower().strip()
     if (color == 'red'):
         print('Correct!')
